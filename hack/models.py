e22f1b579cad138b614458f4b276ad9fa0176e93
LeonardoSilva <apx88.0@gmail.com>
2020-06-26 09:46:01 -0400

    created model

--- hack/models.py
+++ hack/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    content = models.CharField(max_length=1200)
+    issued = models.CharField(max_length=1200)
+
+    def __str__(self):
+        return self.title
