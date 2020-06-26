1af7bac9fd8009aecf23efff348b467e157e0501
Miles <mham7714@gmail.com>
2020-06-26 12:32:19 -0400

    changes to view

--- hack/views.py
+++ hack/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .serializers import ArticleSerializer
 from .models import Article
-<<<<<<< HEAD
 # import os
-=======
-import os
->>>>>>> cfeff56eec7a606a317816ffd39c98d1e6cb4b8c
 
 
 class ArticleList(generics.ListCreateAPIView):
