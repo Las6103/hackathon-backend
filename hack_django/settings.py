dff7c222b5301b7d57af8d8628851ac87cfc7024
Miles <mham7714@gmail.com>
2020-06-26 12:55:49 -0400

    changed database engine in seeting.py

--- hack_django/settings.py
+++ hack_django/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'hack_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hack',
         'USER': 'hackuser',
         'PASSWORD': 'hack',
