6acbd146ecf356ee387b4b3f68dea24a4aa7e87b
Miles <mham7714@gmail.com>
2020-06-26 14:08:17 -0400

    added middleware "whitenoise" for static managment

--- hack_django/settings.py
+++ hack_django/settings.py
@@ -85,9 +85,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hack_django.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
