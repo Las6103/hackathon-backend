e3d960ec8d6d99e9cfe3e6c57524779c19d06290
Miles <mham7714@gmail.com>
2020-06-26 12:23:27 -0400

    updated secret key, everything works now

--- hack_django/settings.py
+++ hack_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '1qbv*%0(2a7wah3(d334f3@&sq7&f0lqh_&t3bjtf((en^=lm8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
