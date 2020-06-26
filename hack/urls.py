b7c6480b921cd4202db080e1fc205c1399bbcdd1
LeonardoSilva <apx88.0@gmail.com>
2020-06-26 10:02:31 -0400

    backend functional

---
+++ hack/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    path('articles/', views.ArticleList.as_view(), name='article_list'),
+    path('artistss/<int:pk>', views.ArticleDetail.as_view(), name='article_detail'),
+]
