cfeff56eec7a606a317816ffd39c98d1e6cb4b8c
Miles <mham7714@gmail.com>
2020-06-26 11:52:24 -0400

    fixing permissions

--- hack/views.py
+++ hack/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .serializers import ArticleSerializer
 from .models import Article
+import os
 
 
 class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = (permissions.IsAuthenticated,)
 
 
 class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = (permissions.IsAuthenticated,)
